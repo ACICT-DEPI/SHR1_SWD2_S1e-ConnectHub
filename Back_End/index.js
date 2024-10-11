5ee6590b802ad87a60a211a26a4c8aff91c30c4b
Ahmed Wagih <ahmedwagih087@gmail.com>
2024-10-12 00:12:53 +0300

    add cors

--- Back_End/index.js
+++ Back_End/index.js
@@ -26,7 +26,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
