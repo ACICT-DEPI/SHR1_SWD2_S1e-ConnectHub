b6c9c88db2193c15af9f320473ac35be538b5585
Ahmed Wagih <ahmedwagih087@gmail.com>
2024-10-12 00:34:58 +0300

    .

--- Back_End/index.js
+++ Back_End/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'https://depi-graduate-project-front.vercel.app/',
+    origin: 'https://depi-graduate-project-front.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
