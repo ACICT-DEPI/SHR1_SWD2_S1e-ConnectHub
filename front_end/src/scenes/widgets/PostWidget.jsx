ed4bc80329a7fa28ada90f2f07383766ea08d75b
Ahmed Wagih <ahmedwagih087@gmail.com>
2024-10-12 11:54:40 +0300

    .

--- front_end/src/scenes/widgets/PostWidget.jsx
+++ front_end/src/scenes/widgets/PostWidget.jsx
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: '0.75rem', marginTop: '0.75rem' }}
-          src={`https://depi-graduate-project-back.vercel.app/assets/${picturePath}`}
+          src={`https://depi-graduate-project-back.vercel.app/assets/info4.jpeg`}
         />
       )}
       <FlexBetween mt="0.25rem">
